ca1c1d527ce71d0db25f24113d979167756a4d26
jumpmanda <amandajanelle12@gmail.com>
2015-11-14 13:37:20 -0800

    Amanda Base Map Project
    Contains all the objects and assets for Tutorial level and Level one. We
    are going to use this as a base for building our game. All scripts and
    art assets should be fully operational before pushing into this folder.
    TLDR: using this as our final game file.

---
+++ Branch Amanda Game Ver 1.0/Assets/Scripts/charMove.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class charMove : MonoBehaviour
+{
+
+    public float moveSpeed;
+    public float jumpHeight;
+    private Rigidbody2D rb;
+    private int jumpCounter = 0;
+    public int maxJumps;
+
+    // Use this for initialization
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            if (jumpCounter < maxJumps)
+            {
+                rb.velocity = new Vector2(0, jumpHeight);
+                jumpCounter++;
+                    
+                if(jumpCounter == maxJumps)
+                {
+                    jumpCounter = 0; 
+                }
+
+            }
+        }
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            rb.velocity = new Vector2(-moveSpeed, rb.velocity.y);
+        }
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            rb.velocity = new Vector2(moveSpeed, rb.velocity.y);
+        }
+    }
+}
